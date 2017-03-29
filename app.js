b7e9373ddc7a522e42e4797109e9570aa2e1c0c9
Mark Noizumi <mark.noizumi@gmail.com>
2017-03-28 20:07:50 -0700

    plotting points

--- app.js
+++ app.js
@@ -4,23 +4,25 @@ var svg = d3.select("svg")
             .attr("width", width)
             .attr("height", height);
 
-var data = [[250, 250], [0, 0], [100, 150], [400, 200], [700, 250], [5000, 400]];
+// var data = [[250, 250], [0, 0], [100, 150], [400, 200], [700, 250], [5000, 400]];
+var data = movies;
+
 var padding = 20
 
 var xMin = d3.min(data, function(d) {
-  return d[0];
+  return d.daysOpen;
 });
 
 var xMax = d3.max(data, function(d) {
-  return d[0];
+  return d.daysOpen;
 });
 
 var yMin = d3.min(data, function(d) {
-  return d[1];
+  return d.total;
 });
 
 var yMax = d3.max(data, function(d) {
-  return d[1];
+  return d.total;
 });
 
 var xScale = d3.scaleLinear()
@@ -31,13 +33,35 @@ var yScale = d3.scaleLinear()
                .domain([yMin, yMax])
                .range([ padding, height - padding]); // [height, 0] - flips it - inverse
 
+var colorScale = d3.scaleLinear()
+                  .domain([0,1])
+                  .range(['red', 'green']);
+
+var tooltip = d3.select("body")
+                .append("div")
+                .attr("class", "tooltip")
+                .style("opacity",0)
+                .style("position","absolute");
+
 svg.selectAll('circle')
   .data(data)
   .enter()
   .append('circle')
-  .attr('cx', function(d) { return xScale(d[0]); })
-  .attr('cy', function(d) { return yScale(d[1]); })
-  .attr('r', function() { return 10; });
+  .attr('cx', function(d) { return xScale(d.daysOpen); })
+  .attr('cy', function(d) { return yScale(d.total); })
+  .attr('r', function(d) { return 5 * d.total / d.openingTotal; })
+  .attr('fill', function(d) {
+    return colorScale(d.freshness);
+  })
+  .on('mouseover', function(d) {
+    console.log(d);
+    console.log(d3.event.screenX,
+                d3.event.screenY);
+    tooltip.text(d.title)
+           .style("opacity", 1)
+           .style("left", d3.event.pageX + "px")
+           .style("top", d3.event.pageY + "px");
+  })
 
 
 // var quotes = [{
